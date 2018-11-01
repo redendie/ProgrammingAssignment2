0d86885ca55a5700aeba8a1fe49ae6e5212dff3e
RemiDen <mrwemi@gmail.com>
2018-11-01 01:16:59 +0100

    commit assignment 2 files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use a closure to build caching capability and avoid calculating
+## values already computed.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## The caching mechanism
+makeCacheMatrix <- function(M = matrix()) {
+        # The cache stores the matrix of interest and
+        # the inverse of this matrix. No calculation happen
+        # here: the inverse matrix is being stored in the
+        # `inverse` variable through a setter function.
+        M.inverse <- NULL
+        set <- function(N) {
+                M <<- N
+                inverse <<- NULL
+        }
+        get <- function() M
+        setinverse <- function(inverse.to.cache) M.inverse <<- inverse.to.cache
+        getinverse <- function() M.inverse
+        
+        # Returns an interface to manage the state of the cache
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of a cache-enabled matrix. The caching logic
+## is operated from here.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        
+        # `inverse` is not null when the computation has already been done.
+        # The cached value is returned.
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        
+        # Otherwise, calculate the inverse here,
+        # and use the setter function from `x` to store
+        # the calculated inverse in `x`.
+        M <- x$get()
+        inverse <- solve(M, ...)
+        x$setinverse(inverse)
+        
+        # Return the inverse of the matrix stored in `x`
+        inverse
 }
+
+
